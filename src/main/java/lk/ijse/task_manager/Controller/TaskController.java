541b34a3b6d84fcca60648320b357cfed6fbfe47
chulakasam <sampathchulaka33872@gmail.com>
2025-04-06 17:44:10 +0530

    task entity create

--- src/main/java/lk/ijse/task_manager/Controller/TaskController.java
+++ src/main/java/lk/ijse/task_manager/Controller/TaskController.java
@@ -1,4 +1,10 @@
 package lk.ijse.task_manager.Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
 public class TaskController {
+
 }
--- src/main/java/lk/ijse/task_manager/Entity/Impl/TaskEntity.java
+++ src/main/java/lk/ijse/task_manager/Entity/Impl/TaskEntity.java
@@ -1,4 +1,25 @@
 package lk.ijse.task_manager.Entity.Impl;
 
-public class TaskEntity {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lk.ijse.task_manager.Entity.SuperEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name="task")
+@Entity
+public class TaskEntity implements SuperEntity {
+    @Id
+    private Long id;
+    private String title;
+    private String description;
+    private String status;
+    private LocalDateTime createdAt;
 }
