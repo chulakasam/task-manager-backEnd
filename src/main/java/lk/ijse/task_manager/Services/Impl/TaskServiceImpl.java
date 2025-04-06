d7871f2a73ae44d2560c98ecdc342b7bf7557db5
chulakasam <sampathchulaka33872@gmail.com>
2025-04-06 17:59:36 +0530

    model mapping add

--- src/main/java/lk/ijse/task_manager/Services/Impl/TaskServiceImpl.java
+++ src/main/java/lk/ijse/task_manager/Services/Impl/TaskServiceImpl.java
@@ -1,4 +1,11 @@
 package lk.ijse.task_manager.Services.Impl;
 
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class TaskServiceImpl {
+
 }
--- src/main/java/lk/ijse/task_manager/Util/Mapping.java
+++ src/main/java/lk/ijse/task_manager/Util/Mapping.java
@@ -1,4 +1,12 @@
 package lk.ijse.task_manager.Util;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Mapping {
+    @Autowired
+    private ModelMapper modelMapper;
+
 }
