d7331a17472c1d8ae86f7a8d2956b727ccaee258
chulakasam <sampathchulaka33872@gmail.com>
2025-04-06 17:55:46 +0530

    custom exception create

--- src/main/java/lk/ijse/task_manager/Exception/TaskNotFoundException.java
+++ src/main/java/lk/ijse/task_manager/Exception/TaskNotFoundException.java
@@ -1,4 +1,13 @@
 package lk.ijse.task_manager.Exception;
 
-public class TaskNotFoundException {
+public class TaskNotFoundException extends RuntimeException {
+    public TaskNotFoundException() {
+
+    }
+    public TaskNotFoundException(String message) {
+
+    }
+    public TaskNotFoundException(String message, Throwable cause) {
+
+    }
 }
