c460c39d7eea52efd2f5000702ca40cc3e48aff8
chulakasam <sampathchulaka33872@gmail.com>
2025-04-06 17:54:11 +0530

    custom status create

--- src/main/java/lk/ijse/task_manager/CustomStatusCode/SelectedTaskErrorStatus.java
+++ src/main/java/lk/ijse/task_manager/CustomStatusCode/SelectedTaskErrorStatus.java
@@ -1,4 +1,14 @@
 package lk.ijse.task_manager.CustomStatusCode;
 
-public class SelectedTaskErrorStatus {
+import lk.ijse.task_manager.Dto.TaskStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class SelectedTaskErrorStatus  implements TaskStatus {
+    private int statusCode;
+    private String statusMessage;
 }
