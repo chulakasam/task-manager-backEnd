0fbc82b1f7d86bc41dd24014e749cb348e0cf997
chulakasam <sampathchulaka33872@gmail.com>
2025-04-06 17:52:30 +0530

    taskdao create

--- src/main/java/lk/ijse/task_manager/Dao/TaskDao.java
+++ src/main/java/lk/ijse/task_manager/Dao/TaskDao.java
@@ -1,4 +1,10 @@
 package lk.ijse.task_manager.Dao;
 
-public interface TaskDao {
+
+import lk.ijse.task_manager.Entity.Impl.TaskEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskDao extends JpaRepository<TaskEntity, Long> {
 }
