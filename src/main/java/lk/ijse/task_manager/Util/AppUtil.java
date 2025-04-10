b0f33394fad543129328ee047b4f293dce574f97
chulakasam <sampathchulaka33872@gmail.com>
2025-04-10 19:07:10 +0530

    fix task Id issue with creating task_id.txt

--- src/main/java/lk/ijse/task_manager/Util/AppUtil.java
+++ src/main/java/lk/ijse/task_manager/Util/AppUtil.java
@@ -1,10 +1,30 @@
 package lk.ijse.task_manager.Util;
 
-import java.util.UUID;
+import java.io.*;
 
 public class AppUtil {
-    private static int counter = 1;
-    public static String  generateTaskId(){
-        return String.format("Task %02d", counter++);
+    private static final String COUNTER_FILE = "task_counter.txt";
+    private static int counter = loadCounter();
+
+    private static int loadCounter() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(COUNTER_FILE))) {
+            return Integer.parseInt(reader.readLine());
+        } catch (IOException | NumberFormatException e) {
+            return 1;
+        }
+    }
+
+    private static void saveCounter() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(COUNTER_FILE))) {
+            writer.write(String.valueOf(counter));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String generateTaskId() {
+        String id = String.format("Task %03d", counter++);
+        saveCounter(); // Save after generating ID
+        return id;
     }
 }
